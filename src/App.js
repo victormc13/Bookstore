37fff1a681c524c1fd575a4b5683db95a45509b1
victormc13 <viktorsb14@gmail.com>
2023-08-19 02:05:01 -0500

    Add Header element inside App component with styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './routes/Home';
+import Categories from './routes/Categories';
 
 function App() {
   return (
     <div className="App">
-      <h1>Bookstore</h1>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/categories" element={<Categories />} />
+      </Routes>
     </div>
   );
 }
