aa4cc3534f26a06dd40645e3b4610293d124c5b4
victormc13 <viktorsb14@gmail.com>
2023-08-25 11:08:04 -0500

    Fix linter error

--- src/redux/categories/categorySlice.js
+++ src/redux/categories/categorySlice.js
@@ -14,6 +14,6 @@ const categorySlice = createSlice({
   },
 });
 
-export const { status } = categorySlice.actions;
+export const { checkStatus } = categorySlice.actions;
 
 export default categorySlice.reducer;
